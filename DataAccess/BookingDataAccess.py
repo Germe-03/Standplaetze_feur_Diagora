521c2960d7c2c5e286c845fc72d1e4c3bdbe58e1
david <david.germann@students.fhnw.ch>
2025-08-29 18:42:43 +0200

    updated BookingDataAccess.py

--- DataAccess/BookingDataAccess.py
+++ DataAccess/BookingDataAccess.py
@@ -5,6 +5,7 @@ from Model.Booking import Booking
 from Model.Location import Location
 from Model.Campaign import Campaign
 from Model.User import User
+from datetime import date, datetime
 
 
 class BookingDataAccess(BaseDataAccess):
@@ -104,3 +105,26 @@ class BookingDataAccess(BaseDataAccess):
         """
         rows = self.fetchall(sql, (city,))
         return [Booking(*row) for row in rows]
+
+    def get_bookings_by_state(self, state: str) -> list[Booking]:
+        sql = """
+        select b.BookingID, b.DateOfBooking, b.DateOfEvent, b.DateOfLastUpdate,
+            b.Price, b.Confirmed, b.LocationID, b.CampaignID, b.UserID
+        from Bookings b
+        join Locations l on l.LocationID = b.LocationID
+        join Cities c on c.CityID = l.CityID
+        join States s on s.StateID = c.StateID
+        where s.Name = ?
+        """
+        rows = self.fetchall(sql, (state,))
+        return [Booking(*row) for row in rows]
+
+    def insert_booking(self, date_of_event: date, price: float, confirmed: bool, location_id: int, cancelled: bool, campaign_id: int, user_id: int ) -> Booking:
+        current_date = date.today()
+        sql = """
+        INSERT INTO Bookings (DateOfBooking, DateOfEvent, DateOfLastUpdate, Price, Confirmed, LocationID, Cancelled, CampaignID, UserID)
+            Values (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """
+
+        new_id, _ = self.execute(sql, (current_date, date_of_event, current_date, price, confirmed, location_id, cancelled, campaign_id, user_id))
+        return Booking(new_id, current_date, date_of_event, current_date, price, confirmed, location_id, cancelled, campaign_id, user_id)
